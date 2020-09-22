7f649063be40fd9b5a1667c1e752c29938b7f669
Azza434 <57290778+Azza434@users.noreply.github.com>
2020-09-22 10:30:56 +0100

    Added about.html to views folder

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
@@ -6,9 +7,15 @@ const server = http.createServer((req, res) => {
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<p>Hello World!</p>');
-    res.write('<p>Hello World Again!</p>');
-    res.end();
+    // send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            //res.write(data);
+            res.end(data);
+        }
+    });    
 });
 
 server.listen(3000, 'localhost', () => {
