c756243bec9b044299549a0f1d52e584eae8a882
Azza434 <57290778+Azza434@users.noreply.github.com>
2020-09-22 13:51:27 +0100

    Add template to 404.ejs

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
 // redirects
@@ -24,6 +24,6 @@ app.get('/about-us', (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
 
